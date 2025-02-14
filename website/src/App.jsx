aacacbe5021fba510d3108270832c35c31928401
greekfreek23 <nicholas@atlasgrowth.ai>
2025-02-14 09:33:59 +0000

    Restored to 'f40c8da1d37a6b286e3d6b94aa9b47c4b817bd51'
    Replit-Restored-To:f40c8da1d37a6b286e3d6b94aa9b47c4b817bd51

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,17 +1,10 @@
 
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MainContent from './components/MainContent';
 import './styles/App.css';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainContent />} />
-      </Routes>
-    </Router>
-  );
+  return <MainContent />;
 }
 
 export default App;
